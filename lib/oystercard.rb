21869d6f83acc78f6fd1eb04da1139c08fb27cd1
Simon <01secn@gmail.com>
2019-02-13 16:25:52 +0000

    Integrated journey class into oystercard

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,13 @@
 
 class Oystercard
-  attr_reader :balance, :journey_history
+  attr_reader :balance, :journey_history, :current_journey
   DEFAULT_LIMIT = 90
   MIN_BALANCE = 1
 
-  def initialize
+  def initialize(journey_class)
     @balance = 0
     @journey_history = []
-    @current_journey = {}
+    @journey_class = journey_class
   end
 
   def top_up(amount)
@@ -20,15 +20,10 @@ class Oystercard
     raise max_exceeded_error if (@balance + amount) > DEFAULT_LIMIT
   end
 
-  def in_journey?
-    !@current_journey[:entry_station].nil?
-  end
-
   def touch_in(station)
     check_has_min_balance
-    @current_journey = {
-      entry_station: station
-    }
+    @current_journey = @journey_class.new
+    @current_journey.start(station)
   end
 
   def check_has_min_balance
@@ -36,18 +31,18 @@ class Oystercard
   end
 
   def touch_out(exit_station)
-    @current_journey[:exit_station] = exit_station
+    if @current_journey == nil then @current_journey = @journey_class.new end
+
+    @current_journey.stop(exit_station)
+
+    deduct(@current_journey.calculate_fare)
+
     @journey_history << @current_journey
-    @current_journey = {}
-    deduct(Journey::FARE)
+    @current_journey = nil
   end
 
   def deduct(amount)
     @balance -= amount
-  end
-
-  def entry_station
-    @current_journey[:entry_station]
   end
 
   private :deduct, :check_has_min_balance, :check_if_max_balance_exceeded
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,6 +4,9 @@ describe Oystercard do
   let (:entry_station_double) { double('entry_station') }
   let (:exit_station_double) { double('exit_station') }
   let (:min_balance) { Oystercard::MIN_BALANCE }
+  let (:journey_double) { double(:journey, start: "", stop: "", calculate_fare: 2) }
+  let (:journey_double_class) { double(:journey_class, new: journey_double) }
+  subject (:station) { described_class.new(journey_double_class) }
 
   it 'has a balance of zero when it is created' do
     expect(subject.balance).to eq(0)
@@ -21,18 +24,6 @@ describe Oystercard do
     expect { subject.top_up(1) }.to raise_error expected_error
   end
 
-  it 'given has balance of 1 or more, updates journey status when touching in' do
-    subject.top_up(min_balance)
-
-    subject.touch_in(entry_station_double)
-    expect(subject.in_journey?).to be true
-  end
-
-  it 'updates journey status when touching out' do
-    subject.touch_out(exit_station_double)
-    expect(subject.in_journey?).to be false
-  end
-
   it 'throws an error on touching in if balance is less than minimum balance' do
     subject.top_up(min_balance - 0.01)
     expected_error = 'Insufficient balance to touch in.'
@@ -40,26 +31,11 @@ describe Oystercard do
     expect { subject.touch_in(entry_station_double) }.to raise_error expected_error
   end
 
-  it 'reduces the card balance by the minimum fare value' do
+  it 'reduces the card balance by the fare specified by the journey' do
     subject.top_up(min_balance)
-    expect { subject.touch_out(exit_station_double) }.to change{subject.balance}.by(-min_balance)
-  end
-
-  it 'will record the entry station when touching in' do
-    subject.top_up(min_balance)
-
-    subject.touch_in(entry_station_double)
-
-    expect(subject.entry_station).to eq(entry_station_double)
-  end
-
-  it 'will forget the entry station when touching out' do
-    subject.top_up(min_balance)
-
-    subject.touch_in(entry_station_double)
-    subject.touch_out(exit_station_double)
-
-    expect(subject.entry_station).to eq(nil)
+    expect { subject.touch_out(exit_station_double) }
+      .to change { subject.balance }
+      .by(-journey_double.calculate_fare)
   end
 
   it 'has an empty journey_history by default' do
@@ -71,5 +47,12 @@ describe Oystercard do
     subject.touch_in(entry_station_double)
     subject.touch_out(exit_station_double)
     expect(subject.journey_history.length).to eq(1)
+  end
+
+  it 'creates and stores a new journey when touching in' do
+    subject.top_up(min_balance)
+    subject.touch_in(entry_station_double)
+
+    expect(subject.current_journey).to eq(journey_double)
   end
 end
